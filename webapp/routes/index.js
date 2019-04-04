28bc27dbd29d7aeb20d3d8b91214b147c3fe823f
root <root@draws.localhost>
2019-04-04 15:38:44 -0700

    Adding Application Help

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -7,5 +7,11 @@ router.get('/', function(req, res, next) {
 	res.render('index', { layout: 'layout', controls: io.controls, title: 'DRAWS™ Manager', stats: io.systemstats});
 });
 
+
+router.get('/application-support', function(req, res, next) {
+//	console.log("-" + JSON.stringify(io.controls,null,4));
+	res.render('app-support', { layout: 'layout', controls: io.controls, title: 'Application Help', stats: io.systemstats});
+});
+
 module.exports = router;
 
